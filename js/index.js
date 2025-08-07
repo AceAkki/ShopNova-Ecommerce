d9759c700b44e875a21a4637f9794cb903a86cfd
Akshay P (A-K-SHA-Y) <140274081+AceAkki@users.noreply.github.com>
2025-08-07 23:56:47 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,14 +5,36 @@ document.addEventListener("DOMContentLoaded",  inIt)
 
 
 function inIt () {
-  let formElm = document.querySelector("#loginForm");
+  let header = document.querySelector("#header");
+
+  fetch("header.html")
+  .then((response) => response.text())
+  .then((data) => {
+      header.innerHTML = data;
+
+      let headerSize = document.querySelector(".navbar").getBoundingClientRect().height;
+      const root = document.documentElement;
+      root.style.setProperty('--header-size', `${headerSize}px`);
+
+      let loginBtn = document.querySelector("#loginBtn");
+      loginBtn.addEventListener("click", () => {
+          let loginForm =  document.querySelector(".login-wrap");
+          let productContainer =  document.querySelector(".main-container");
+          let loginDisplay = window.getComputedStyle(loginForm).display;
+          loginForm.style.display = (loginDisplay === "none") ? "block" : "none" ;
+          productContainer.style.display = (loginForm.style.display === "block") ?  "none" : "block";
+      })
+  })
   
+  let formElm = document.querySelector("#loginForm");
+  // intializes APICalls class
   const callAPI = new APICalls({
     formElement : formElm,
     errorClass : "api-error"
   })
   loginUser (formElm)
   populateProducts() 
+
 
   function loginUser (formElm) {
     let loginData; 
@@ -29,7 +51,6 @@ function inIt () {
       }
     })
   }
-
   async function populateProducts() {
     let productsData = await callAPI.fetchData ('https://dummyjson.com/products?limit=0');
     const paginationMain = new Pagination({
@@ -79,77 +100,51 @@ function generateItems (data) {
   return item;
 }
 
+// to generate rating
 function generateRating(rating) {
-  if (rating >= 4.50) {
-    return 5
-  }
-  else if (rating >= 4 && rating < 4.50) {
-    // console.log( rating, "4 full and half")
-    return 4.5
-  }
-  else if (rating >= 3.50 && rating < 4) {
-    // console.log(Math.ceil(rating), rating, "4")
-    return 4
-  }
-  else if (rating >= 3 && rating < 3.50) {
-    // console.log( rating, "3 full and half")
-    return 3.5
-  }
-  else if (rating >= 2.50 && rating < 3) {
-    // console.log(Math.ceil(rating), rating, "3")
-    return 3
-  }
-  else if (rating >= 2 && rating < 2.50) {
-    // console.log( rating, "2 full and half")
-    return 2.5
-  }
-  else if (rating >= 1.50 && rating < 2) {
-    // console.log(Math.ceil(rating), rating, "2")
-    return 2
-  }
-  else if (rating >= 1 && rating < 1.50) {
-    // console.log( rating, "1 full and half")
-    return 1.5
-  }
-  else if (rating >= 1.50 && rating < 2) {
-    // console.log(Math.ceil(rating), rating, "1")
-    return 1
-  } else {
-    // console.log(Math.ceil(rating), rating, "0")
-    return 0    
-  }
+  // hashmaps maps rating with conditions
+  const map = new Map([
+    [5, rating >= 4.50],
+    [4.5, rating >= 4 && rating < 4.50],
+    [4, rating >= 3.50 && rating < 4],
+    [3.5, rating >= 3 && rating < 3.50],
+    [3, rating >= 2.50 && rating < 3],
+    [2.5 ,rating >= 2 && rating < 2.50],
+    [2, rating >= 1.50 && rating < 2],
+    [1.5, rating >= 1 && rating < 1.50],
+    [1, rating >= 0.50 && rating < 1],
+  ]);
 
+  // on truthy value to condition we return rating
+  for (const key of map) {
+    if (key[1]) {
+      return key[0];      
+    }
+  };
 }
 
+// to create star and add
 function createStar (num, parent) {
   let maxStar = 5;
-  let starArr;
-  let emptyArr;
-  if (Number.isInteger(num)) {
-    let emptyNum = maxStar - num;
-    starArr = [...Array(num).keys()];
-    for(let i = 0; i < starArr.length; i++){
-      addFillStar(parent)
-    }
-    if (emptyNum > 0) {
-      emptyArr = [...Array(emptyNum).keys()];
-      for(let i = 0; i < emptyArr.length; i++){
-        addEmptyStar(parent)
-      }
-    }
-  } else {
-    let fillNum = num - 0.5;
-    let emptyNum = maxStar - Math.ceil(num);
-    starArr = [...Array(fillNum).keys()];
-    for(let i = 0; i < starArr.length; i++){
-      addFillStar(parent)
-    }
-    addHalfStar(parent)
-    if (emptyNum > 0) {
-      emptyArr = [...Array(emptyNum).keys()];
-      for(let i = 0; i < emptyArr.length; i++){
-        addEmptyStar(parent)
-      }
+  let fillStarArr;
+  let emptyStarArr;
+  let emptyStarNum;
+  let fillStarNum = num;
+
+  // on truthy value for num emptyStar num and fill star nums are updated while on falsy only empty stat num
+  !Number.isInteger(num) ? (emptyStarNum = maxStar - Math.ceil(num), fillStarNum = num - 0.5 ): (emptyStarNum = maxStar - num);
+  // fill star num is added
+  fillStarArr = [...Array(fillStarNum).keys()];
+  for(let i = 0; i < fillStarArr.length; i++){
+    addFillStar(parent)
+  }
+  // on truthy value half star is added
+  if (!Number.isInteger(num))  addHalfStar(parent);
+  // empty stars are added if num is bigger
+  if (emptyStarNum > 0) {
+    emptyStarArr = [...Array(emptyStarNum).keys()];
+    for(let i = 0; i < emptyStarArr.length; i++){
+      addEmptyStar(parent)
     }
   }
 }
